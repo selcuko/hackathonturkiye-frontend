c8bfbcf1217903d411e4e342b23cd0081b9814c3
EARES <info@emreakbaki.com>
2020-05-17 16:42:59 +0300

    Lazy loading add to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from "./pages/home/home.component";
 import { NotfoundComponent } from "./pages/notfound/notfound.component";
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // Redirect to `home`
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to `home`
   { path: '**', component: NotfoundComponent },  // Wrong route for a 404 page
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
@@ -13,7 +12,6 @@ import { GlobalErrorHandler } from './services/common/global-error-handler';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     NotfoundComponent,
     LoadingSpinnerComponent
   ],
