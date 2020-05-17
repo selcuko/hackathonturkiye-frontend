33574be65db1b53e942e89b89ed39c05a423f8f7
EARES <info@emreakbaki.com>
2020-05-17 15:12:41 +0300

    test dev branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { NotfoundComponent } from "./notfound/notfound.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `home`
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // Redirect to `home`
   { path: '**', component: NotfoundComponent },  // Wrong route for a 404 page
 ];
 
