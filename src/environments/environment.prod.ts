7360a4803b14dd3557f37cd8670e9a80b53e6a4b
Uğur Atmaca <uguratmaca87@hotmail.com>
2020-07-05 16:26:34 +0300

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/',
+  apiUrl: 'http://209.97.180.252:8000/',
   //apiUrl: 'http://omerselcuk.engineer:8000/',
   trackCode: 'UA-109031383-1',
 };
