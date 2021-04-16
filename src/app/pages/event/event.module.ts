935817d3d8c652d7bbe3fde8ef849cf9aaf555c8
Uğur Atmaca <uguratmaca87@hotmail.com>
2021-04-16 10:29:06 +0300

    appcent

--- src/app/pages/event/event.module.ts
+++ src/app/pages/event/event.module.ts
@@ -4,15 +4,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { EventRoutingModule } from './event-routing.module';
 import { EventComponent } from './event.component';
-import { DisqusModule } from 'ngx-disqus';
 
 @NgModule({
   declarations: [EventComponent],
   imports: [
     CommonModule,
     EventRoutingModule,
-    FormsModule,
-    DisqusModule
+    FormsModule
   ]
 })
 export class EventModule { }
